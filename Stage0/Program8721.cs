837dbc0a4506c385c4daa168a2aeaeeea8cbd184
shimon khakshour <shimony2017@gmail.com>
2024-11-10 13:36:05 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program8721.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8721();
+            Welcome7403();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome7403();
+        private static void Welcome8721()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
